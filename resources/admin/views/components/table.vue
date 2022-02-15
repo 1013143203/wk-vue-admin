e0763e4bc35aacf1befd12ca64e20c9f9f4489b9
weiweiwei <1013143203@qq.com>
2022-02-15 16:33:23 +0800

    '2'

--- resources/admin/views/components/table.vue
+++ resources/admin/views/components/table.vue
@@ -12,6 +12,7 @@
           placement="top"
           width="160"
           v-model="popoverVisible">
+          <div class="table-tool-column"></div>
           <el-checkbox
             v-model="cols.all"
             @change="(val) => colsChange(val,'all')"
@@ -22,9 +23,12 @@
             v-model="cols.data"
             @change="(value) => colsChange(value)"
           >
-            <div class="colsbox" v-for="col in cols.lst">
-              <el-checkbox :label="col.label" :key="col.label">{{col.label}}</el-checkbox>
-            </div>
+            <draggable  filter=".move" @sort="toolColumnSort" :move="toolColumnMove">
+              <div class="colsbox" v-for="col in cols.lst">
+                <div class="table-tool-column" :class="(col.type=='btn'?'move':'')">::</div>
+                <el-checkbox :label="col.label" :key="col.label">{{col.label}}</el-checkbox>
+              </div>
+            </draggable>
           </el-checkbox-group>
           <div class="item" slot="reference">
             <el-button size="mini" icon="el-icon-s-operation" round circle></el-button>
@@ -34,7 +38,7 @@
     </el-row>
     <el-row :gutter="15">
       <el-table
-        :data="table.lst"
+        :data="lst"
         size="medium"
         @cell-click="cellClick"
         @row-click="rowClick"
@@ -47,7 +51,7 @@
           <!-- 是否多选 -->
           <el-table-column
             type="selection"
-            :key="`v.label${k}`"
+            :key="`col_${k}`"
             v-if="options.mutiSelect && v.type == 'mutiSelect' && v.show"
           ></el-table-column>
           <!-- 常规列数据 -->
@@ -55,7 +59,7 @@
             :label="v.label"
             :width="v.width"
             v-if="!v.type && v.show"
-            :key="`v.label${k}`"
+            :key="`col_${k}`"
             :prop="v.prop"
             :formatter="v.formatter"
           >
@@ -65,7 +69,7 @@
             :label="v.label"
             :width="v.width"
             v-if="v.type == 'icon' && v.show"
-            :key="`v.label${k}`"
+            :key="`col_${k}`"
             :prop="v.prop"
           >
             <template slot-scope="scope" v-if="scope.row.icon">
@@ -79,7 +83,7 @@
             :label="v.label"
             :width="v.width"
             v-if="v.type == 'switch' && v.show"
-            :key="`v.type${k}`"
+            :key="`col_${k}`"
           >
             <template slot-scope="scope">
               <el-switch
@@ -98,7 +102,7 @@
             :label="v.label"
             :width="v.width"
             v-if="v.type == 'image' && v.show"
-            :key="`v.label${k}`"
+            :key="`col_${k}`"
           >
             <template slot-scope="scope" :formatter="v.format">
               <img
@@ -114,7 +118,7 @@
             :label="v.label"
             :width="v.width"
             v-if="v.type == 'btn' && v.show"
-            :key="`v.type${k}`"
+            :key="`col_${k}`"
             fixed="right"
           >
             <!-- 处理自定义操作中的按钮 -->
@@ -147,167 +151,198 @@
 </template>
 
 <script>
+  import draggable from 'vuedraggable'
   import Sortable from 'sortablejs';
-export default {
-  name: "wk-table",
-  components: { Sortable },
-  props: {
-    table:{
-      type:Object,
-      default:()=>{}
-    },
-    btn:{
-      type:Array,
-      default:()=>[]
-    },
-    expand: {
-      type: Boolean,
-      default:false,
-    },
-  },
-  watch:{
-    expand:{
-      handler(val, oldVal){
-        this.refreshTable = false;
-        this.$nextTick(() => {
-          this.expands=val
-          this.refreshTable = true;
-        })
+  export default {
+    name: "wk-table",
+    components: { draggable },
+    props: {
+      table:{
+        type:Object,
+        default:()=>{}
       },
-      deep:true //true 深度监听
+      btn:{
+        type:Array,
+        default:()=>[]
+      },
+      expand: {
+        type: Boolean,
+        default:false,
+      },
     },
-    table:{
-      handler(val, oldVal){
-        let data = [],cols = val.cols
-        cols = [{label:'列选项',type:'mutiSelect'}].concat(cols)
-        for (let item of cols) {
-          item.show = true
-          data.push(item.label);
-        }
-        this.cols.data = data;
-        this.cols.lst = cols
-
-        const that = this
-        this.$nextTick(() => {
-          const el = that.$refs.dragTable.$el.querySelectorAll('.el-table__header-wrapper tr')[0]
-           Sortable.create(el, {
-           ghostClass:'sortable-ghost',
-             animation: 180,
-             delay: 0,
-             onEnd: evt => {
-              const oldItem = that.cols.lst[evt.oldIndex]
-              that.cols.lst.splice(evt.oldIndex, 1)
-              that.cols.lst.splice(evt.newIndex, 0, oldItem)
-             }
+    watch:{
+      expand:{
+        handler(val, oldVal){
+          this.refreshTable = false;
+          this.$nextTick(() => {
+            this.expands=val
+            this.refreshTable = true;
           })
-        })
+        },
+        deep:true //true 深度监听
       },
-      deep:true //true 深度监听
-    }
-  },
-  mounted(){
-  },
-  data() {
-    return {
-      cols:{
-        data:[],
-        lst:[],
-        all: true,
-        isIndeterminate:false,
-      },
-      currentPage:1,
-      expands:false,
-      refreshTable:true,
-      popoverVisible:false,
-      options: {
-        mutiSelect: true, //boolean 是否多选
-        isindex: false, //boolean 是否展示序列号
-        stripe: true, //boolean 斑马纹
-        border: true, //boolean 纵向边框
-        size: "medium", //String  medium / small / mini  table尺寸
-        fit: true, //自动撑开
-      },
-    };
-  },
-  methods: {
-    colsChange(val,flag=''){
-      let data = []
-      if (flag=='all'){
-        if (val){
-          for (let item of this.cols.lst) {
+      table:{
+        handler(val, oldVal){
+          let data = [],cols = val.cols
+          cols = [{label:'列选项',type:'mutiSelect'}].concat(cols)
+          for (let item of cols) {
             item.show = true
             data.push(item.label);
           }
-        }else{
-          for (let item of this.cols.lst) {
-            item.show = false
-          }
-        }
-        this.cols.isIndeterminate = false;
-      }else{
-        data = val;
-        let length = this.cols.lst.length
-        let checkedCount = val.length;
-        for (let item of this.cols.lst){
-          item.show = val.indexOf(item.label) > -1
-        }
-        this.cols.all = checkedCount === length;
-        this.cols.isIndeterminate = checkedCount > 0 && checkedCount < length;
-      }
+          this.cols.data = data;
+          this.cols.lst = cols
+          this.lst = val.lst
 
-      this.cols.data = data
-    },
-    getTable(){
-      this.$parent.$parent.index()
-    },
-    iconLoad(icon,c){
-      if (icon){
-        return icon.includes(c)
+        },
+        deep:true //true 深度监听
+      },
+      cols:{
+        handler(val, oldVal){
+          if (val.lst.length){
+            const that = this
+            this.$nextTick(() => {
+              this.columnDrop(that)
+            })
+          }
+        },
+        deep:true //true 深度监听
       }
     },
-    cellClick(row, column, cell, event) {},
-    rowClick(row, column, cell, event) {
-      this.$emit("rowClick", row, column, cell, event);
+    mounted(){
     },
-    // 序列号相关
-    indexMethod(index) {
-      return index + 1;
+    data() {
+      return {
+        cols:{
+          data:[],
+          lst:[],
+          all: true,
+          isIndeterminate:false,
+        },
+        lst:[],
+        currentPage:1,
+        expands:false,
+        refreshTable:true,
+        popoverVisible:false,
+        options: {
+          mutiSelect: true, //boolean 是否多选
+          isindex: false, //boolean 是否展示序列号
+          stripe: true, //boolean 斑马纹
+          border: true, //boolean 纵向边框
+          size: "medium", //String  medium / small / mini  table尺寸
+          fit: true, //自动撑开
+        },
+      };
     },
-    // 分页相关
-    pageChange(val) {
-      this.currentPage = val
-      this.$emit("pageChange", val);
+    methods: {
+      toolColumnSort(evt){
+        const $colsLst = this.cols.lst
+        const $lst = this.lst
+        this.columnSort(this,evt,$colsLst,$lst)
+      },
+      toolColumnMove(evt){
+        console.log(123)
+      },
+      columnDrop(that){
+        const wrapperTr = that.$refs.dragTable.$el.querySelector('.el-table__header-wrapper tr')
+        const $colsLst = that.cols.lst
+        const $lst = that.lst
+
+        Sortable.create(wrapperTr, {
+          animation: 180,
+          delay: 0,
+          onEnd: evt => {
+            that.columnSort(that,evt,$colsLst,$lst)
+          }
+        });
+      },
+      columnSort(that,evt,$colsLst,$lst){
+        const oldItem = $colsLst[evt.oldIndex];
+        $colsLst.splice(evt.oldIndex, 1);
+        $colsLst.splice(evt.newIndex, 0, oldItem);
+        that.lst = that.cols.lst = [];
+        setTimeout(() => {
+          that.cols.lst = $colsLst
+          that.lst = $lst
+        }, 5);
+      },
+      colsChange(val,flag=''){
+        let data = []
+        if (flag=='all'){
+          if (val){
+            for (let item of this.cols.lst) {
+              item.show = true
+              data.push(item.label);
+            }
+          }else{
+            for (let item of this.cols.lst) {
+              item.show = false
+            }
+          }
+          this.cols.isIndeterminate = false;
+        }else{
+          data = val;
+          let length = this.cols.lst.length
+          let checkedCount = val.length;
+          for (let item of this.cols.lst){
+            item.show = val.indexOf(item.label) > -1
+          }
+          this.cols.all = checkedCount === length;
+          this.cols.isIndeterminate = checkedCount > 0 && checkedCount < length;
+        }
+
+        this.cols.data = data
+      },
+      getTable(){
+        this.$parent.$parent.index()
+      },
+      iconLoad(icon,c){
+        if (icon){
+          return icon.includes(c)
+        }
+      },
+      cellClick(row, column, cell, event) {},
+      rowClick(row, column, cell, event) {
+        this.$emit("rowClick", row, column, cell, event);
+      },
+      // 序列号相关
+      indexMethod(index) {
+        return index + 1;
+      },
+      // 分页相关
+      pageChange(val) {
+        this.currentPage = val
+        this.$emit("pageChange", val);
+      },
     },
-  },
-};
+  };
 </script>
 <style scoped>
-.sub-el-icon {
-  color: currentColor;
-  width: 1em;
-  height: 1em;
-}
-.el-link+.el-link{
-  margin-left: 10px;
-}
-.table-tool .el-col{
-  float: left;
-  margin-right: 6px;
-}
-.table-tool .tool{
-  float: right;
-}
-.table-tool .tool .item{
-  display: inline-block;
-  margin-left: 6px;
-}
-.el-divider{
-  margin: 10px 0;
-}
-.colsbox{
-  margin-top: 6px;
-}
-.colsbox .icon{
-  width: 20px;display: inline-block;font-size: 14px;color: #606266;cursor:move;
-}
+  .sub-el-icon {
+    color: currentColor;
+    width: 1em;
+    height: 1em;
+  }
+  .el-link+.el-link{
+    margin-left: 10px;
+  }
+  .table-tool .el-col{
+    float: left;
+    margin-right: 6px;
+  }
+  .table-tool .tool{
+    float: right;
+  }
+  .table-tool .tool .item{
+    display: inline-block;
+    margin-left: 6px;
+  }
+  .el-divider{
+    margin: 10px 0;
+  }
+  .colsbox{
+    margin-top: 6px;
+  }
+  .table-tool-column{
+    width: 20px;display: inline-block;font-size: 14px;color: #606266;cursor:move;
+  }
 </style>
